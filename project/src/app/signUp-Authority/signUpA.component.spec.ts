f809af4c1967ffdbe6ae4cf04d3eeb216e90b6f0
Salma Saeed <101990421+SalmaSaeed29@users.noreply.github.com>
2022-12-04 16:17:03 +0200

    Add files via upload

---
+++ project/src/app/signUp-Authority/signUpA.component.spec.ts
@@ -0,0 +1,37 @@
+<html>
+
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="/src/app/signUp/signUpP.css">
+</head>
+
+<body>
+
+    <div class="containerSign">
+        <div class="signContent ">
+            <div class="card border-danger p-5" style="max-width: 400px;">
+                <p>Sign up</p>
+                <div class="mb-3">
+                    <label for="id" class="form-label">ID</label>
+                    <input type="number" class="form-control" id="id" placeholder="14525411658522">
+                </div>
+                <div class="mb-3">
+                    <label for="pass" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="pass" placeholder="*************">
+                </div>
+                <button type="submit" class="btn btn-danger" style="max-width: 100px;">Submit</button>
+                <button type="button" class="btn btn-link btn-go">sign up for authoritize</button>
+                <button type="button" class="btn btn-link btn-go">sign in for authoritize</button>
+                <button type="button" class="btn btn-link btn-go">sign in for individual</button>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>

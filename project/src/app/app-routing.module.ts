4e198c838867a42346b0139401b0455624102c92
Salma Saeed <101990421+SalmaSaeed29@users.noreply.github.com>
2022-12-04 16:19:09 +0200

    Add files via upload

---
+++ project/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { signInAComponent } from './signIn-Authority/signInA.component'
+import { signInIComponent } from './signIn-Individual/signInI.component'
+
+const routes: Routes = [
+  { path: '' ,redirectTo: 'home' , pathMatch: 'full' },
+  { path: 'home', component:HomeComponent},
+  { path: 'signInA', component:signInAComponent},
+  { path: 'signInI', component:signInIComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ project/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import{ HttpClientModule }from'@angular/common/http'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HomeComponent } from './home/home.component';
+import { signInAComponent } from './signIn-Authority/signInA.component'
+import { signInIComponent } from './signIn-Individual/signInI.component'
+
+@NgModule({
+  declarations: [
+    AppComponent, 
+    HomeComponent,
+    signInAComponent,
+    signInIComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

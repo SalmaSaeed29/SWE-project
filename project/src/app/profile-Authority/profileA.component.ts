932ff04dabb41066d36347623a5194e485e03273
Salma Saeed <101990421+SalmaSaeed29@users.noreply.github.com>
2022-12-06 01:01:42 +0200

    Add files via upload

--- project/src/app/profile-Authority/profileA.component.ts
+++ project/src/app/profile-Authority/profileA.component.ts
@@ -1,1 +1,15 @@
+import { Component, OnInit } from '@angular/core';
 
+@Component({
+  selector: 'app-profile-Authority',
+  templateUrl: './profileA.component.html',
+  styleUrls: ['./profileA.component.css']
+})
+export class profileAComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

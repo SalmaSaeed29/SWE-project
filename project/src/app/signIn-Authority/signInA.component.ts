72e74d58181e4ff2057d0fb75a251471384cef6f
Salma Saeed <101990421+SalmaSaeed29@users.noreply.github.com>
2022-12-06 00:45:19 +0200

    Add files via upload

--- project/src/app/signIn-Authority/signInA.component.ts
+++ project/src/app/signIn-Authority/signInA.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signIn-Authority',
@@ -7,9 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class signInAComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+  }
+
+  Email: any = ''
+  Password: any = ''
+  returnedValue: any
+
+  addEmail(){
+    const inputEmail = (<HTMLInputElement>document.getElementById('email'))
+    this.Email = inputEmail.value
+    console.log(this.Email)
+  }
+
+  addPassword(){
+    const inputPassword = (<HTMLInputElement>document.getElementById('password'))
+    this.Password = inputPassword.value
+    console.log(this.Password)
+  }
+
+  SIGNIN_A(Email:string, Password:string){
+    this.http.get('http://localhost:3030/savior/signInA',{
+      responseType:'text',
+      params:{
+        email: Email,
+        password: Password
+      },
+      observe:'response'
+    }).subscribe(response=>{
+      this.returnedValue = response.body
+      console.log(this.returnedValue)
+      if(this.returnedValue === ''){
+        alert("WRONG email or password!")
+      }
+      else{
+        this.router.navigateByUrl("/profileA")
+      }
+    })
+  }
+
+  signInA(){
+    this.addEmail()
+    this.addPassword()
+    this.SIGNIN_A(this.Email, this.Password);
+    console.log(this.returnedValue)
   }
 
 }

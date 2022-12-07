02fcec3f3df47dd7fe672c4f7f80694600fa0d7b
Salma Saeed <101990421+SalmaSaeed29@users.noreply.github.com>
2022-12-07 10:21:31 +0200

    Add files via upload

--- project/src/app/signIn-Individual/signInI.component.ts
+++ project/src/app/signIn-Individual/signInI.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signIn-Individual',
@@ -7,9 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class signInIComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+  }
+
+  ID: any = ''
+  Password: any = ''
+
+  response: any = ''
+
+  addID(){
+    const inputID = (<HTMLInputElement>document.getElementById('id'))
+    this.ID = inputID.value
+    console.log(this.ID)
+  }
+
+  addPassword(){
+    const inputPassword = (<HTMLInputElement>document.getElementById('password'))
+    this.Password = inputPassword.value
+    console.log(this.Password)
+  }
+
+  SIGNIN_I(ID:string, Password:string){
+    this.http.get('http://localhost:6060/Savior/signInI',{
+      responseType:'text',
+      params:{
+        id: ID,
+        password: Password
+      },
+      observe:'response'
+    }).subscribe(response=>{
+      this.response = response.body
+      console.log(this.response)
+      if(this.response === "False"){
+        alert("WRONG id or password!")
+      }
+      else{
+        this.router.navigateByUrl("/profileI")
+      }
+    })
+  }
+
+  signInA(){
+    this.addID()
+    this.addPassword()
+    this.SIGNIN_I(this.ID, this.Password);
   }
 
 }
